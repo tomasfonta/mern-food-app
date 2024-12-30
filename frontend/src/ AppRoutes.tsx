15bd88bd770f4287d085c0fbbfbc42b48b6734a5
tomas.fontanarrosa <tomas.fontanarrosa@edreamsodigeo.com>
2024-12-30 20:52:06 +0100

    Make Bitcoin Great Again

---
+++ frontend/src/ AppRoutes.tsx
@@ -0,0 +1,16 @@
+
+import { Navigate, Route, Routes } from "react-router-dom"
+import Layout from "./layout/Layout";
+
+const AppRoutes = () => {
+	return (
+
+		<Routes>
+			<Route path="/" element={<Layout> HOME PAGE</Layout>} />
+			<Route path="/user-profile" element={<span>USER PROFILE </span>} />
+			<Route path="*" element={<Navigate to="/" />} />
+		</Routes>
+	)
+};
+
+export default AppRoutes;
